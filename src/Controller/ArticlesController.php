bb44ff73e96f708ae9c794e94d132675f7c382ea
Dylan <dylngae@outlook.com>
2019-12-19 16:43:55 +0100

    firstCommit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Article;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+/**
+ * @Route("/article", name="article")
+ */
+
+class ArticlesController extends AbstractController
+{
+
+    /**
+     * @Route("/create", name="create")
+     */
+
+    public function create(Request $request)
+    {
+        $article = new Article;
+
+        $form = $this->createFormBuilder($article)
+
+            ->add('title', TextType::class)
+            ->add('image', TextType::class)
+            ->add('content', TextareaType::class)
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer un article',
+                'attr' => [
+                    'class' => 'btn-success'
+                ]
+            ])
+            ->getForm();
+
+            $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dateTime = new \DateTime();
+            $article->setDate($dateTime);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+        }
+
+        return $this->render('article/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+        
+    }
+}
